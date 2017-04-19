f21c43fa2fc64ee04335606b6e424007dd316e4f
ozhe <oleg.zhelenkov@hotmail.com>
2017-04-20 02:16:24 +0300

    [LWDEV-1767] Add redirect from root to swagger UI

---
+++ src/LykkePublicAPI/Controllers/RootController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace LykkePublicAPI.Controllers
+{
+    [Route("")]
+    public class RootController : Controller
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Redirect("~/swagger/ui");
+        }
+    }
+}

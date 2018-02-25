e898312a45874a72ed472b6c754db6e3abebeeac
Nikoloz Gamkrelidze <>
2018-02-25 16:16:28 +0400

    method added and nuget packages updated

--- src/LykkePublicAPI/Controllers/TradesController.cs
+++ src/LykkePublicAPI/Controllers/TradesController.cs
@@ -1,6 +1,10 @@
-﻿using System.Threading.Tasks;
+﻿using System.Linq;
+using System.Threading.Tasks;
 using Core.Domain.Exchange;
+using Lykke.Service.Assets.Client.Custom;
+using Lykke.Service.TradesAdapter.Client;
 using LykkePublicAPI.Models;
+using LykkePublicAPI.Models.Trades;
 using Microsoft.AspNetCore.Mvc;
 
 namespace LykkePublicAPI.Controllers
@@ -10,12 +14,15 @@ namespace LykkePublicAPI.Controllers
     {
         private const int MaxTrades = 500;
         private readonly ITradesCommonRepository _tradesCommonRepository;
+        private readonly ITradesAdapterClient _tradesAdapterClient;
 
         public TradesController(
-            ITradesCommonRepository tradesCommonRepository
+            ITradesCommonRepository tradesCommonRepository,
+            ITradesAdapterClient tradesAdapterClient
             )
         {
             _tradesCommonRepository = tradesCommonRepository;
+            _tradesAdapterClient = tradesAdapterClient;
         }
 
         /// <summary>
@@ -43,6 +50,32 @@ namespace LykkePublicAPI.Controllers
             var trades = await _tradesCommonRepository.GetLastTrades(assetPair, n);
 
             return Ok(trades.ToApiModel());
+        }
+        
+        /// <summary>
+        /// Provides latest trades for given asset pair
+        /// </summary>
+        /// <param name="assetPairId">Id of asset pair</param>
+        /// <param name="skip">How many items to skip</param>
+        /// <param name="take">How many items to take</param>
+        /// <returns></returns>
+        [HttpGet("{assetPairId}")]
+        public async Task<IActionResult> GetTrades(string assetPairId,
+            [FromQuery] int skip,
+            [FromQuery] int take)
+        {
+            var trades = await _tradesAdapterClient.GetTradesByAssetPairIdAsync(assetPairId, skip, take);
+
+            if (trades.Error != null)
+            {
+                return BadRequest(new ApiError
+                {
+                    Code = ErrorCodes.InvalidInput,
+                    Msg = trades.Error.Message
+                });
+            }
+            
+            return Ok(trades.Records.Select(x => x.ToResponseModel()));
         }
     }
 }

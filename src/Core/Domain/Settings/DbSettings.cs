96231fde729f27ea6ec01cf304b59d24b703013a
Dmitry Samodov <samodov.dmitry@gmail.com>
2018-04-24 10:24:42 +0300

    use balances service

--- src/Core/Domain/Settings/DbSettings.cs
+++ src/Core/Domain/Settings/DbSettings.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using Lykke.Service.Balances.Client;
 using Lykke.Service.TradesAdapter.Client;
 using Lykke.SettingsReader.Attributes;
 
@@ -16,6 +17,7 @@ namespace Core.Domain.Settings
         public MarketProfileServiceClientSettings MarketProfileServiceClient { get; set; }
         public RegistrationServiceClientSettings RegistrationServiceClient { get; set; }
         public TradesAdapterServiceClientSettings TradesAdapterServiceClient { get; set; }
+        public BalancesServiceClientSettings BalancesServiceClient { get; set; }
     }
 
     public class RateLimitSettings

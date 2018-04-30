9610613d8b3acd281b40fedd5bbcd46588d734ac
Dmitry Samodov <samodov.dmitry@gmail.com>
2018-04-30 14:08:27 +0300

    removed BalancesConnString from settings

--- src/Core/Domain/Settings/DbSettings.cs
+++ src/Core/Domain/Settings/DbSettings.cs
@@ -98,8 +98,6 @@ namespace Core.Domain.Settings
         [AzureTableCheck]
         public string HTradesConnString { get; set; }
         [AzureTableCheck]
-        public string BalancesInfoConnString { get; set; }
-        [AzureTableCheck]
         public string HLiquidityConnString { get; set; }
         //[AzureTableCheck]
         public string LogsConnString { get; set; }
